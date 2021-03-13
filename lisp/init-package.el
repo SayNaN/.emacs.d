99008a2855ab77b120140a811ddee08f1f16a568
zhaiyj <zhaiyj@sogou-inc.com>
2021-03-13 18:27:09 +0800

    20210313 update

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -18,22 +18,9 @@
   :if (memq window-system '(mac ns x))
   :init (exec-path-from-shell-initialize))
 
-;; Settings for C-a behavior
-(use-package crux
-  :bind (("C-a" . crux-move-beginning-of-line)
-         ("C-c ^" . crux-top-join-line)
-         ("C-," . crux-find-user-init-file)
-         ("C-S-d" . crux-duplicate-current-line-or-region)
-         ("C-S-k" . crux-smart-kill-line))) ; We can use C-S-<Backspace> instead.
-
 ;; Hungry Delete - delete multi spaces with one <delete> key
 (use-package hungry-delete
   :config (global-hungry-delete-mode))
-
-;; drag-stuff - move lines up/down
-(use-package drag-stuff
-  :bind (("<M-up>". drag-stuff-up)
-         ("<M-down>" . drag-stuff-down)))
 
 ;; Settings for company
 (use-package company
@@ -145,8 +132,27 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 ;; Settings for jump windows, use M-NUM to switch
-(use-package ace-window
-  :bind (("M-o" . 'ace-window)))
+(use-package winum
+  :init
+  ;; {{ move focus between sub-windows
+  (setq winum-keymap
+        (let ((map (make-sparse-keymap)))
+          (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+          (define-key map (kbd "M-1") 'winum-select-window-1)
+          (define-key map (kbd "M-2") 'winum-select-window-2)
+          (define-key map (kbd "M-3") 'winum-select-window-3)
+          (define-key map (kbd "M-4") 'winum-select-window-4)
+          (define-key map (kbd "M-5") 'winum-select-window-5)
+          (define-key map (kbd "M-6") 'winum-select-window-6)
+          (define-key map (kbd "M-7") 'winum-select-window-7)
+          (define-key map (kbd "M-8") 'winum-select-window-8)
+          map))
+  (winum-mode)
+  :config
+  (setq winum-format "%s")
+  (setq winum-mode-line-position 0)
+  (set-face-attribute 'winum-face nil :foreground "DeepPink" :underline "DeepPink" :weight 'bold)
+  )
 
 ;; Restart emacs
 ;; Use custom interactive func "cabins/reload-init-file" instead.
